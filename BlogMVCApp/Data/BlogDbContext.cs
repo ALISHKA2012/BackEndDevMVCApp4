42aec7fd4845dc565ee7e645e540aa1c1c44b729
Ali <U_orucova@hotmail.com>
2021-04-18 23:31:30 +0400

    initial commit

---
+++ BlogMVCApp/Data/BlogDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using BlogMVCApp.Models;
+
+namespace BlogMVCApp.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext():base("myblogDbConnection")
+        {
+
+        }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
