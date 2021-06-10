1c891eedee0a6e853c117639991b4fcc98ad33e5
Ulviyya Rahimova <U_Orucova@hotmail.com>
2021-06-11 00:43:50 +0400

    Login Page Validation started

--- BlogMVCApp/Areas/Admin/Controllers/AccountController.cs
+++ BlogMVCApp/Areas/Admin/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
 ﻿using BlogMVCApp.Areas.Admin.Data;
+using BlogMVCApp.Data;
+using BlogMVCApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,14 @@ namespace BlogMVCApp.Areas.Admin.Controllers
 {
     public class AccountController : Controller
     {
+        private BlogDbContext _blogDbContext;
+        private int _itemsPerPage;
+        public AccountController()
+        {
+            _blogDbContext = new BlogDbContext();
+            _itemsPerPage = 4;
+        }
+
         [HttpGet]
         public ActionResult Index()
         {
@@ -20,7 +30,16 @@ namespace BlogMVCApp.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-
+                User user = _blogDbContext.Users.Where(x => x.Email == model.Email && x.Password == model.Password).FirstOrDefault();
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Given email or password is wrong!");
+                    return RedirectToAction("Index", "Account");
+                }
+                else
+                {
+                        
+                }
             }
             return View();
         }
