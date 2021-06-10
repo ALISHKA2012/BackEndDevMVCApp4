24395e7ffa8088c55f8d225f7d8fa56bfd8c6295
Ulviyya Rahimova <U_Orucova@hotmail.com>
2021-06-10 21:04:24 +0400

    Login Page And Admin

---
+++ BlogMVCApp/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using BlogMVCApp.Areas.Admin.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogMVCApp.Areas.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View();
+        }
+    }
+}
